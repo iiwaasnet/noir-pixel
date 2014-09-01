e077cdd1eb7b8e61b2c6e7c4e61ccc15e9984d6f
iiwaasnet <givasena@gmail.com>
2014-09-01 21:06:31 +0200

    JS errors logging with Api.Logging

--- app/Api.Logging/Startup.cs
+++ app/Api.Logging/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Owin;
 
 [assembly: OwinStartup(typeof(Api.Logging.Startup))]
@@ -13,6 +14,7 @@ namespace Api.Logging
         public void Configuration(IAppBuilder app)
         {
             //ConfigureAuth(app);
+            app.UseCors(new CorsOptions());
         }
     }
 }
