be4c9b4d257e0d4e91ec007caa224174211edd25
iiwaasnet <givasena@gmail.com>
2014-09-01 19:34:57 +0200

    Api.Logging project added

---
+++ app/Api.Logging/Controllers/LoggingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Api.Logging.Controllers
+{
+    [RoutePrefix("logging")]
+    public class LoggingController : ApiController
+    {
+        [Route("error")]
+        public async Task<IHttpActionResult> Error()
+        {
+            return Ok();
+        }
+    }
+}
