dae329537caa0a55d7f5f6e5be42de5c2c083a3c
iiwaasnet <givasena@gmail.com>
2014-09-02 13:58:12 +0200

    JS logging refactoring

--- app/Web/app/src/Logging/Logging.js
+++ app/Web/app/src/Logging/Logging.js
@@ -4,69 +4,37 @@
             return { print: printStackTrace };
         }
     ])
-    .factory('ExceptionLogging', [
-        '$log', '$window', '$httpBackend', 'Trace', function($log, $window, $httpBackend, Trace) {
-            function error(exception, cause) {
-                $log.error.apply($log, arguments);
-
-                try {
-                    var errorMessage = exception.toString();
-                    var stackTrace = Trace.print({ exception: exception });
-
-                    $httpBackend('POST',
-                        'http://api.logging.noir-pixel.com/logging/error',
-                        angular.toJson({
-                            url: $window.location.href,
-                            message: errorMessage,
-                            type: 'exception',
-                            stackTrace: stackTrace,
-                            cause: (cause || '')
-                        }),
-                        function(status, resp, headerString) {
-                            if (status !== 200) {
-                                $log.log(status, resp, headerString);
-                            }
-                        },
-                        {
-                            'Content-Type': 'application/json',
-                        }
-                    );
-
-                } catch (e) {
-                    $log.warn("Error server-side logging failed");
-                    $log.log(e);
-                }
-            }
-
-            return error;
-        }
-    ])
     .factory('ApplicationLogging', [
-            '$log', '$window', '$httpBackend', 'Trace' ,function($log, $window, $httpBackend, Trace) {
+            '$log', '$window', '$httpBackend', 'Trace', function($log, $window, $httpBackend, Trace) {
                 var logger = {
-                    error: function(error) {
+                    error: function(exception, cause) {
                         $log.error.apply($log, arguments);
 
-                        var message = error,
-                            stackTrace = '';
-                        if (error && typeof error === 'object') {
-                            message = error.toString();
-                            stackTrace = Trace.print({ exception: error });
-                        }
-
-                        $httpBackend('POST',
-                            'http://api.logging.noir-pixel.com/logging/error',
-                            angular.toJson({
-                                url: $window.location.href,
-                                message: message,
-                                stackTrace: stackTrace,
-                                type: 'error'
-                            }),
-                            responseLogging,
-                            {
-                                'Content-Type': 'application/json',
+                        try {
+                            var errorMessage = (exception || '').toString();
+                            var stackTrace = [];
+                            if (typeof exception === 'object') {
+                                stackTrace = Trace.print({ exception: exception });
                             }
-                        );
+                            $httpBackend('POST',
+                                'http://api.logging.noir-pixel.com/logging/error',
+                                angular.toJson({
+                                    url: $window.location.href,
+                                    message: errorMessage,
+                                    type: 'exception',
+                                    stackTrace: stackTrace,
+                                    cause: (cause || '')
+                                }),
+                                responseLogging,
+                                {
+                                    'Content-Type': 'application/json',
+                                }
+                            );
+
+                        } catch (e) {
+                            $log.warn("Error server-side logging failed");
+                            $log.log(e);
+                        }
                     },
                     debug: function(message) {
                         $log.log.apply($log, arguments);
@@ -84,8 +52,6 @@
                             }
                         );
                     }
-
-                    
                 };
 
                 function responseLogging(status, resp, headerString) {
@@ -98,6 +64,11 @@
             }
         ]
     )
+    .factory('ExceptionLogging', [
+        'ApplicationLogging', function(ApplicationLogging) {
+            return ApplicationLogging.error;
+        }
+    ])
     .provider('$exceptionHandler', {
             $get: function(ExceptionLogging) {
                 return (ExceptionLogging);
