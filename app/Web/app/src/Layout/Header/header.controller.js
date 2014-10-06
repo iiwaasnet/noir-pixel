49e094fb6a4619a42dbb91ba9a8ff947098e4e28
iiwaasnet <givasena@gmail.com>
2014-10-06 20:53:50 +0200

    Angular one-time binding test

--- app/Web/app/src/Layout/Header/header.controller.js
+++ app/Web/app/src/Layout/Header/header.controller.js
@@ -8,6 +8,23 @@
 
     function headerController(MainMenu, $scope) {
         var ctrl = this;
-        ctrl.mainMenu = MainMenu.getMainMenu();
+        $scope.mainMenu = MainMenu.getMainMenu();
+        $scope.add = add;
+        $scope.title = 'bla';
+
+        function add() {
+            $scope.title = 'bla-bla';
+            $scope.mainMenu[0].text = 'bla';
+            //$scope.mainMenu = [
+            //{
+            //    text: 'Bla1',
+            //    uri: 'gallery'
+            //},
+            //    {
+            //        text: 'Bla2',
+            //        uri: 'gallery'
+            //    }
+            //];
+        }
     };
 })();
