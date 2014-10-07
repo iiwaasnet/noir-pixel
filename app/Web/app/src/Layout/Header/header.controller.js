d3e5f6a5578bfcdeee86e1bdbead8ef9d6ed4f76
sivasenko <sergey.ivasenko@bwinparty.com>
2014-10-07 08:40:56 +0200

    MainMenu

--- app/Web/app/src/Layout/Header/header.controller.js
+++ app/Web/app/src/Layout/Header/header.controller.js
@@ -4,27 +4,10 @@
     angular.module('np.layout')
         .controller('HeaderController', headerController);
 
-    headerController.$injector = ['MainMenu', '$scope'];
+    headerController.$injector = ['MainMenu'];
 
-    function headerController(MainMenu, $scope) {
+    function headerController(MainMenu) {
         var ctrl = this;
-        $scope.mainMenu = MainMenu.getMainMenu();
-        $scope.add = add;
-        $scope.title = 'bla';
-
-        function add() {
-            $scope.title = 'bla-bla';
-            $scope.mainMenu[0].text = 'bla';
-            //$scope.mainMenu = [
-            //{
-            //    text: 'Bla1',
-            //    uri: 'gallery'
-            //},
-            //    {
-            //        text: 'Bla2',
-            //        uri: 'gallery'
-            //    }
-            //];
-        }
+        ctrl.mainMenu = MainMenu.getMainMenu();
     };
 })();
