671f134487c69924ea47baea9ddf065be7331fbe
sivasenko <sergey.ivasenko@bwinparty.com>
2014-10-15 09:49:23 +0200

    TRY: Lazy-loading

--- app/Web/app/src/Localization/strings.service.js
+++ app/Web/app/src/Localization/strings.service.js
@@ -11,7 +11,6 @@
     function stringsService($q, $http, $rootScope, $window, $interval, localStorageService, Config, ApplicationLogging, Moment) {
         var service = this,
             dictionary = {},
-            stringsLoaded = false,
             langStorageKey = 'currentLang',
             language = localStorageService.get(langStorageKey)
                 || ($window.navigator.userLanguage || $window.navigator.language).split('-')[0];
@@ -20,52 +19,70 @@
         service.init = init;
         service.setCurrentLanguage = setCurrentLanguage;
         service.getCurrentLanguage = getCurrentLanguage;
-        service.loadStrings = loadStrings;
+        //service.loadStrings = loadStrings;
         service.getLocalizedString = getLocalizedString;
 
         function init() {
             var deferred = $q.defer();
 
-            service.setCurrentLanguage(service.getCurrentLanguage());
+            //service.setCurrentLanguage(service.getCurrentLanguage());
             checkStringVersions()
-                .success(invalidateLocalCache)
-                .error(function(msg) { invalidateLocalCache([]); })
-                .success()
-                .error();
+                .then(invalidateLocalCache)
+                .then(function() {
+                    deferred.resolve(true);
+                }, function() {
+                    deferred.reject(false);
+                });
             //scheduleCacheInvalidation();
 
             return deferred.promise;
         }
 
         function invalidateLocalCache(versions) {
-            var deferred = $q.defer();
+            var deferred = $q.defer(),
+                promises = [];
 
-            versions.forEach(checkLocaleStrings);
-
-            Object.keys(dictionary).forEach(function(lang) {
-                var cache = dictionary[lang];
-                var versionInfo = data.versions.filter(function(el) {
-                    return el.locale === cache.locale;
-                })[0];
-                //if (versionInfo && versionInfo.version !== cache.version) {
-                //TODO: Decide on the strings versioning
-                loadStringsForLocale(cache.language, deferred);
-                //}
+            versions.forEach(function(lang) {
+                promises.push(checkInvalidateLocaleStrings(lang));
             });
+
+            $q.all(promises)
+                .then(function() {
+                    removeUnusedStrings(versions);
+                    deferred.resolve(true);
+                }, function() {
+                    deferred.reject(false);
+                });
 
             return deferred.promise;
         }
 
-        function checkLocaleStrings(lang) {
-            var deferred = $q.defer();
-            var promises = new 
+        function removeUnusedStrings(versions) {
+            var unusedLocales = getUnusedLocales(versions);
 
-            var cache = dictionary[lang.locale];
+            unusedLocales.forEach(function(lang) {
+                removeStringsFromStorage(lang);
+            });
+        }
+
+        function getUnusedLocales(versions) {
+            var cachedLocales = getStoredLocales();
+            var unusedLocales = cachedLocales.filter(function(el) {
+                return !versions.filter(function(v) {
+                    return v.locale === el;
+                }).length;
+            });
+
+            return unusedLocales;
+        }
+
+        function checkInvalidateLocaleStrings(lang) {
+            var cache = readStringsFromStorage(lang);
+
             if (!cache || cache.version !== lang.version) {
                 return loadStringsForLocale(lang.locale);
             }
-
-            return $q.when(true);
+            return $q.resolve(true);
         }
 
         function setCurrentLanguage(value) {
@@ -103,55 +120,49 @@
             return '';
         }
 
-        function scheduleCacheInvalidation() {
-            var deferred = $q.defer();
+        //function scheduleCacheInvalidation() {
+        //    var deferred = $q.defer();
 
-            var interval = Moment.duration(Config.strings.invalidationTimeout).asMilliseconds();
-            $interval(function() { checkStringVersions(deferred); }, interval, 0, false);
+        //    var interval = Moment.duration(Config.strings.invalidationTimeout).asMilliseconds();
+        //    $interval(function() { checkStringVersions(deferred); }, interval, 0, false);
 
-            return deferred.promise;
-        }
+        //    return deferred.promise;
+        //}
 
         function checkStringVersions() {
             var deferred = $q.defer();
 
             $http({ method: "GET", url: Config.strings.versionsUri, cache: false })
-                .success(function(data) { deferred.resolve(data.versions); })
-                .error(function(msg) { deferred.reject(msg); });
+                .success(function(data) {
+                    deferred.resolve(data.versions);
+                })
+                .error(function() {
+                    deferred.reject([]);
+                });
 
             return deferred.promise;
         }
 
-
-        function getVersionsSuccess(data, deferred) {
-            Object.keys(dictionary).forEach(function(lang) {
-                var cache = dictionary[lang];
-                var versionInfo = data.versions.filter(function(el) {
-                    return el.locale === cache.locale;
-                })[0];
-                //if (versionInfo && versionInfo.version !== cache.version) {
-                //TODO: Decide on the strings versioning
-                loadStringsForLocale(cache.language, deferred);
-                //}
-            });
-        }
-
         function loadStringsForLocale(locale) {
             return $http({ method: "GET", url: Config.strings.localizedUri + locale, cache: false })
-                .success(function(data) { getStringsSuccess(locale, data); })
-                .error(function() { ApplicationLogging.error('Failed loading Strings for language ' + locale + '!'); });
+                .success(function(data) {
+                    saveStringsToStorage(locale, data);
+                })
+                .error(function() {
+                    ApplicationLogging.error('Failed loading Strings for language ' + locale + '!');
+                });
         }
 
-        function getStringsFromCache() {
-            var cache = angular.fromJson(localStorageService.get(getStringsStorageKey()));
-            if (!!cache && Object.keys(cache.strings).length > 0) {
-                return cache;
-            }
+        //function getStringsFromCache(lang) {
+        //    var cache = angular.fromJson(localStorageService.get(getStringsStorageKey(lang)));
+        //    if (!!cache && Object.keys(cache.strings).length > 0) {
+        //        return cache;
+        //    }
 
-            return null;
-        }
+        //    return null;
+        //}
 
-        function getStringsSuccess(lang, data, deferred) {
+        function saveStringsToStorage(requestedLocale, data) {
             var strings = {};
             data.strings.forEach(function(el) {
                 strings[el.key] = el.value;
@@ -159,24 +170,69 @@
 
             var cache = {
                 locale: data.locale,
-                language: lang,
+                language: requestedLocale,
                 version: data.version,
                 strings: strings
             };
-            dictionary[lang] = cache;
-            stringsLoaded = true;
 
-            localStorageService.set(getStringsStorageKey(), angular.toJson(cache));
+            writeStringsToStorage(cache);
 
             $rootScope.$broadcast('stringsUpdated');
 
-            if (lang !== data.locale) {
-                ApplicationLogging.warn('No Strings defined for language ' + lang + '! Locale ' + data.locale + ' used as fall-back.');
+            if (requestedLocale !== data.locale) {
+                ApplicationLogging.warn('No Strings defined for language ' + requestedLocale + '! Locale ' + data.locale + ' used as fall-back.');
             }
         }
 
-        function getStringsStorageKey() {
-            return 'strings-' + service.getCurrentLanguage();
+        function writeStringsToStorage(data) {
+            addStoredLocales(data.local);
+            localStorageService.set(getStringsStorageKey(data.locale), angular.toJson(data));
+        }
+
+        function readStringsFromStorage(lang) {
+            var cache = angular.fromJson(localStorageService.get(getStringsStorageKey(lang)));
+            if (!!cache && Object.keys(cache.strings).length > 0) {
+                return cache;
+            }
+
+            return null;
+        }
+
+        function removeStringsFromStorage(lang) {
+            localStorageService.remove(getStringsStorageKey(lang));
+            removeStoredLocales(lang);
+        }
+
+        function getStoredLocales() {
+            return angular.fromJson(localStorageService.get(getLocaleListStorageKey()))
+                || [];
+        }
+
+        function addStoredLocales(locale) {
+            var locales = getStoredLocales();
+
+            if (locales.indexOf(locale) === -1) {
+                locales.push(locale);
+                localStorageService.set(getLocaleListStorageKey(), angular.toJson(locales));
+            }
+        }
+
+        function removeStoredLocales(locale) {
+            var locales = getStoredLocales();
+
+            var index = locales.indexOf(locale);
+            if (~index) {
+                locales.splice(index, 1);
+                localStorageService.set(getLocaleListStorageKey(), angular.toJson(locales));
+            }
+        }
+
+        function getStringsStorageKey(locale) {
+            return 'strings-' + locale;
+        }
+
+        function getLocaleListStorageKey() {
+            return 'strings-localeList';
         }
     }
 })();
