768debbdcc2ca4134ee6b1475b0e590aa62eaa96
sivasenko <sergey.ivasenko@bwinparty.com>
2014-10-15 14:14:39 +0200

    Refactoring

--- app/Api.Logging/Global.asax.cs
+++ app/Api.Logging/Global.asax.cs
@@ -3,7 +3,6 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
 
 namespace Api.Logging
--- app/Api.Logging/Startup.cs
+++ app/Api.Logging/Startup.cs
@@ -1,6 +1,11 @@
-﻿using Api.Logging;
+﻿using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Api.Logging;
+using Autofac.Integration.WebApi;
 using Microsoft.Owin;
 using Microsoft.Owin.Cors;
+using Newtonsoft.Json.Serialization;
 using Owin;
 
 [assembly: OwinStartup(typeof (Startup))]
@@ -11,8 +16,20 @@ namespace Api.Logging
     {
         public void Configuration(IAppBuilder app)
         {
-            //ConfigureAuth(app);
             app.UseAutofacMiddleware(DependencyInjection.GetContainer());
+
+            //ConfigureAuth(app);
+
+            var config = new HttpConfiguration
+                         {
+                             DependencyResolver = new AutofacWebApiDependencyResolver(DependencyInjection.GetContainer())
+                         };
+            // TODO: Do the same in Web project instead of custom Json result
+            var formatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            formatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            app.UseWebApi(config);
+
             app.UseCors(new CorsOptions());
         }
     }
