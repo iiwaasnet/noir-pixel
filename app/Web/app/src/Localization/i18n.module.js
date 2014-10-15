776c9d2565bcdae38cf2e3ea2f3a673285f8a035
iiwaasnet <givasena@gmail.com>
2014-10-15 20:35:05 +0200

    Added localStorage keys prefix

--- app/Web/app/src/Localization/i18n.module.js
+++ app/Web/app/src/Localization/i18n.module.js
@@ -2,5 +2,12 @@
     'use strict';
 
     angular
-        .module('np.i18n', ['np.logging', 'np.config', 'np.utils', 'LocalStorageModule']);
+        .module('np.i18n', ['np.logging', 'np.config', 'np.utils', 'LocalStorageModule'])
+        .config(config);
+
+    config.$injector = ['localStorageServiceProvider'];
+
+    function config(localStorageServiceProvider) {
+        localStorageServiceProvider.prefix = 'np';
+    }
 })();
