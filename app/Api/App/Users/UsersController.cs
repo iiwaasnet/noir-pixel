5405b81b1fc349c818a025cd4907beda4397c3ee
sivasenko <sergey.ivasenko@bwinparty.com>
2014-10-22 14:30:25 +0200

    UsersController added

---
+++ app/Api/App/Users/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Api.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace Api.App.Users
+{
+    [Authorize]
+    [RoutePrefix("users")]
+    public class UsersController : ApiController
+    {
+        private readonly ApplicationUserManager userManager;
+        private readonly ISecureDataFormat<AuthenticationTicket> accessTokenFormat;
+
+        public UsersController(ApplicationUserManager userManager,
+                               ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
+        {
+            this.userManager = userManager;
+            this.accessTokenFormat = accessTokenFormat;
+        }
+
+        [AllowAnonymous]
+        [Route("register")]
+        [HttpPost]
+        public async Task<IHttpActionResult> Register(RegisterBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
+            }
+
+            return Ok();
+        }
+    }
+}
