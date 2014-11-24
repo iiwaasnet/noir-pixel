dd760a49d92407a8b8566a5a1665e8467fd221be
sivasenko <sergey.ivasenko@bwinparty.com>
2014-11-24 09:58:19 +0100

    Added progress cleanup by timeout

--- app/Web/app/src/progress/progress.service.js
+++ app/Web/app/src/progress/progress.service.js
@@ -1,22 +1,36 @@
-﻿(function () {
+﻿(function() {
     'use strict';
 
     angular.module('np.progress')
         .service('Progress', progressService);
 
-    progressService.$inject = ['NProgress'];
+    progressService.$inject = ['$interval', 'NProgress'];
 
-    function progressService(NProgress) {
-        var srv = this;
+    function progressService($interval, NProgress) {
+        var srv = this,
+            stopCallTimeout = 15 * 1000,
+            timer = undefined;
         srv.start = start;
         srv.stop = stop;
 
         function start() {
+            stop();
+
+            timer = $interval(stop, stopCallTimeout);
             NProgress.start();
         }
 
         function stop() {
             NProgress.done();
+            cancelTimer();
+
+        }
+
+        function cancelTimer() {
+            if (timer) {
+                $interval.cancel(timer);
+                timer = undefined;
+            }
         }
     }
 })();
