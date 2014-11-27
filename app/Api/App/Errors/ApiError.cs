2ad3b1298efb9e1a5f8fbc600c4be7f333d47c19
iiwaasnet <givasena@gmail.com>
2014-11-27 21:23:42 +0100

    ApiError

---
+++ app/Api/App/Errors/ApiError.cs
@@ -0,0 +1,16 @@
+﻿namespace Api.App.Errors
+{
+    public class ApiError
+    {
+        public StringResource Main { get; set; }
+        public StringResource Aux { get; set; }
+
+        public class StringResource
+        {
+            public string Text { get; set; }
+            public string Id { get; set; }
+        }     
+    }
+
+    
+}
