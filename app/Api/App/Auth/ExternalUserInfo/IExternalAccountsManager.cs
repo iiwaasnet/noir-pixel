a3a5bfe1fedd3ba353080decd627ed41e72816cb
sivasenko <sergey.ivasenko@bwinparty.com>
2014-12-01 09:40:41 +0100

    Twitter auth

--- app/Api/App/Auth/ExternalUserInfo/IExternalAccountsManager.cs
+++ app/Api/App/Auth/ExternalUserInfo/IExternalAccountsManager.cs
@@ -4,6 +4,7 @@ namespace Api.App.Auth.ExternalUserInfo
 {
     public interface IExternalAccountsManager
     {
+        //TODO: Introduce parameter object
         Task<ExternalUserInfo> GetUserInfo(string provider, string userId, string accessToken, string accessTokenSecret);
 
         Task<ParsedExternalAccessToken> VerfiyAccessToken(string provider, string accessToken, string accessTokenSecret);
