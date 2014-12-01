5f51cf7bf221e7e0c8a1f75c15292dd6a8a54c01
iiwaasnet <givasena@gmail.com>
2014-12-01 20:36:54 +0100

    Removed unused code

--- app/Api/App/Auth/ExternalUserInfo/Yahoo/YahooAuthProvider.cs
+++ app/Api/App/Auth/ExternalUserInfo/Yahoo/YahooAuthProvider.cs
@@ -17,22 +17,5 @@ namespace Api.App.Auth.ExternalUserInfo.Yahoo
         {
             return Task.FromResult<object>(null);
         }
-
-        //public Task Authenticated(JY.Owin.Security.Yahoo.YahooAuthenticatedContext context)
-        //{
-        //    context.Identity.AddClaim(new Claim("ExternalAccessToken", context.AccessToken));
-        //    context.Identity.AddClaim(new Claim("AccessTokenSecret", context.AccessTokenSecret));
-        //    return Task.FromResult<object>(null);
-        //}
-
-        //public Task ReturnEndpoint(JY.Owin.Security.Yahoo.YahooReturnEndpointContext context)
-        //{
-        //    return Task.FromResult<object>(null);
-        //}
-
-        //public void ApplyRedirect(YahooApplyRedirectContext context)
-        //{
-        //    context.Response.Redirect(context.RedirectUri);
-        //}
     }
 }
