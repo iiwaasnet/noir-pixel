8a8f77f237783a5501d54dd0ba320a11aa7a76b0
iiwaasnet <givasena@gmail.com>
2014-12-02 22:00:17 +0100

    API error handling

---
+++ app/Api/App/Errors/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http.ModelBinding;
+
+namespace Api.App.Errors.Extensions
+{
+    public static class ModelStateExtensions
+    {
+        public static ValidationError ToValidationError(this ModelStateDictionary modelState)
+        {
+        }
+    }
+}
