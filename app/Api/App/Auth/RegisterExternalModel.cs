ecde83ae79480d1cf7ab1caf92e670bb1d3164bd
sivasenko <sergey.ivasenko@bwinparty.com>
2014-12-03 15:20:01 +0100

    API error handling

--- app/Api/App/Auth/RegisterExternalModel.cs
+++ app/Api/App/Auth/RegisterExternalModel.cs
@@ -7,7 +7,8 @@ namespace Api.App.Auth
     {
         [Required(ErrorMessage = ApiErrors.Validation.RequiredValue)]
         public string Provider { get; set; }
-        [Required(ErrorMessage = ApiErrors.Validation.RequiredValue)]
+        //[Required(ErrorMessage = ApiErrors.Validation.RequiredValue)]
+        [Api.Validation.MinLengthAttribute(100, ErrorMessageResourceName = ApiErrors.Validation.RequiredValue)]
         public string ExternalAccessToken { get; set; }
         public string AccessTokenSecret { get; set; }
     }
