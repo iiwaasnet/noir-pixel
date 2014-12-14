0945e0d8b893c49e5e546db0d3a0f7c88825df6d
iiwaasnet <givasena@gmail.com>
2014-12-14 17:51:14 +0100

    UsersController

--- app/Api/App/Auth/ApplicationUser.cs
+++ app/Api/App/Auth/ApplicationUser.cs
@@ -5,15 +5,12 @@ using Microsoft.AspNet.Identity;
 
 namespace Api.App.Auth
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
             // TODO: Add custom user claims here
-            userIdentity.AddClaim(new Claim("user", userIdentity.Name));
-            //userIdentity.AddClaim(new Claim("role", "bla"));
 
             return userIdentity;
         }
---
+++ app/Api/App/Users/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace Api.App.Users
+{
+    [Authorize]
+    [RoutePrefix("users")]
+    public class UsersController : ApiBaseController
+    {
+        [AllowAnonymous]
+        [Route("{userName}")]
+        [HttpGet]
+        public Task<IHttpActionResult> GetUserInfo(string userName)
+        {
+            if (User.Identity.IsAuthenticated && User.Identity.GetUserName().Equals(userName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                var i = 1;
+            }
+
+            return null;
+        }
+    }
+}
