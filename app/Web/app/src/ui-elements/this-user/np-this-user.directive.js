6038cfe57cc45e2a236f22143c79fdbffde2f2b1
iiwaasnet <givasena@gmail.com>
2015-01-08 21:03:07 +0100

    np-this-user

--- app/Web/app/src/ui-elements/this-user/np-this-user.directive.js
+++ app/Web/app/src/ui-elements/this-user/np-this-user.directive.js
@@ -4,26 +4,40 @@
     angular.module('np.ui-elements')
         .directive('npThisUser', thisUserDirective);
 
-    thisUserDirective.$inject = ['User'];
+    thisUserDirective.$inject = ['$templateFactory', '$compile', '$rootScope', 'User'];
 
-    function thisUserDirective(User) {
+    function thisUserDirective($templateFactory, $compile, $rootScope, User) {
         var dir = {
             restrict: 'EA',
             scope: {
                 displayName: '@',
-                img: '@'
+                image: '@'
             },
+            transclude: true,
             templateUrl: '/app/src/ui-elements/this-user/this-user.html',
             link: link
         };
 
         return dir;
 
-        function link(scope, element, attrs) {
-            debugger;
+        function link(scope, element, attrs, ctrl, transcludeFn) {
             var userData = User.getUserData();
-            scope.img = scope.img || userData.thumbnail;
+            scope.image = scope.image || userData.thumbnail;
             scope.displayName = scope.displayName || userData.fullName || userData.userName;
+            scope.image = '';
+            transcludeFn(scope, cloneFunc);
+
+            function cloneFunc(clone, _) {
+                $templateFactory.fromUrl(dir.templateUrl)
+                    .then(function(template) {
+                        template = angular.element(template);
+                        var transcluded = angular.element(template[0].querySelector('[transclude]'));
+                        var parent = transcluded.parent();
+                        parent.append(clone.append(transcluded));
+                        $compile(template)(scope);
+                        element.replaceWith(template);
+                    });
+            }
         }
     }
 })();
