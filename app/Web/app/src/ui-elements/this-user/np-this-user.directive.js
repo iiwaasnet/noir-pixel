ca6410ead479f6f8789a371d662b3d41650b33c8
iiwaasnet <givasena@gmail.com>
2015-01-08 21:33:18 +0100

    np-this-user

--- app/Web/app/src/ui-elements/this-user/np-this-user.directive.js
+++ app/Web/app/src/ui-elements/this-user/np-this-user.directive.js
@@ -24,19 +24,52 @@
             var userData = User.getUserData();
             scope.image = scope.image || userData.thumbnail;
             scope.displayName = scope.displayName || userData.fullName || userData.userName;
-            scope.image = '';
+            //scope.image = '';
+            var transclusionScope,
+                transcludedContent = [];
             transcludeFn(scope, cloneFunc);
 
-            function cloneFunc(clone, _) {
+            function cloneFunc(clone, transScope) {
                 $templateFactory.fromUrl(dir.templateUrl)
                     .then(function(template) {
                         template = angular.element(template);
-                        var transcluded = angular.element(template[0].querySelector('[transclude]'));
-                        var parent = transcluded.parent();
-                        parent.append(clone.append(transcluded));
+                        var transcluded = template[0].querySelectorAll('[transclude]');
+
+                        transclusionScope = transScope;
+                        transcludedContent = cloneElement(clone, transcluded.length);
+
+                        angular.forEach(transcluded, function(el, i) {
+                            debugger;
+                            var content = transcludedContent[i];
+                            el = angular.element(el);
+                            var parent = el.parent();
+                            parent.append(content.append(el));
+                        });
+
                         $compile(template)(scope);
                         element.replaceWith(template);
+
+                        element.on('$destroy', cleanup);
                     });
+            }
+
+            function cloneElement(clone, times) {
+                var clones = [clone];
+                for (var i = 0; i < times - 1; i++) {
+                    clones.push(clone.clone(false));
+                }
+
+                return clones;
+            }
+
+            function cleanup() {
+                debugger;
+                if (transclusionScope) {
+                    transclusionScope.$destroy();
+                }
+                angular.forEach(transcludedContent, function(el) {
+                    el.remove();
+                });
             }
         }
     }
