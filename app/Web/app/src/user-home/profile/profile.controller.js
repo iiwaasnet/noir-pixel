d7ddce386068efbdfc60e4507b18badeb7db1822
iiwaasnet <givasena@gmail.com>
2015-01-11 00:43:58 +0100

    User home

--- app/Web/app/src/user-home/profile/profile.controller.js
+++ app/Web/app/src/user-home/profile/profile.controller.js
@@ -4,7 +4,26 @@
     angular.module('np.user-home')
         .controller('ProfileController', profileController);
 
-    function profileController() {
-        
+    profileController.$inject = ['$rootScope', '$scope', 'States'];
+
+    function profileController($rootScope, $scope, States) {
+        var unsubscribe;
+
+        activate();
+
+        function activate() {
+            $scope.$on('$destroy', cleanup);
+            unsubscribe = $rootScope.$on('$stateChangeStart', stateChangeStart);
+        }
+
+        function stateChangeStart(event, toState, toParams, fromState, fromParams) {
+            if (fromState.name === States.UserHome.Profile.Name) {
+                event.preventDefault();
+            }
+        }
+
+        function cleanup() {
+            unsubscribe && unsubscribe();
+        }
     }
 })();
