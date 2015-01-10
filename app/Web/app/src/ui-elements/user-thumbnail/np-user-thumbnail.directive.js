a6a5143326200986c8548800853beca7301f331a
iiwaasnet <givasena@gmail.com>
2015-01-10 16:59:15 +0100

    replaced block with inline element, so that thumbnail could be directly wrapped by ony other element

--- app/Web/app/src/ui-elements/user-thumbnail/np-user-thumbnail.directive.js
+++ app/Web/app/src/ui-elements/user-thumbnail/np-user-thumbnail.directive.js
@@ -13,17 +13,14 @@
                 displayName: '@',
                 image: '@'
             },
-            transclude: true,
             templateUrl: '/app/src/ui-elements/user-thumbnail/user-thumbnail.html',
             link: link
         };
 
         return dir;
 
-        function link(scope, element, attrs, ctrl, transcludeFn) {
-            var transclusionScope,
-                transcludedContent = [],
-                userData = User.getUserData() || {},
+        function link(scope, element, attrs) {
+            var userData = User.getUserData() || {},
                 thisUser = attrs.thisUser !== undefined;
 
             if (thisUser) {
@@ -32,55 +29,6 @@
             } else {
                 scope.image = scope.image;
                 scope.displayName = scope.displayName;
-            }
-            
-
-            if (transcludeFn) {
-                transcludeFn(scope, cloneFunc);
-            }
-
-            function cloneFunc(clone, transScope) {
-                $templateFactory.fromUrl(dir.templateUrl)
-                    .then(function(template) { transcludeDirective(template, clone, transScope); });
-            }
-
-            function transcludeDirective(template, clone, transScope) {
-                template = angular.element(template);
-                var transcluded = template[0].querySelectorAll('[transclude]');
-
-                transclusionScope = transScope;
-                transcludedContent = cloneElement(clone, transcluded.length);
-
-                angular.forEach(transcluded, transcludeElement);
-
-                $compile(template)(scope);
-
-                element.on('$destroy', cleanup);
-            }
-
-            function transcludeElement(el, i) {
-                var content = transcludedContent[i];
-                el = angular.element(el);
-                var parent = el.parent();
-                parent.append(content.append(el));
-            }
-
-            function cloneElement(clone, times) {
-                var clones = [clone];
-                for (var i = 0; i < times - 1; i++) {
-                    clones.push(clone.clone(false));
-                }
-
-                return clones;
-            }
-
-            function cleanup() {
-                if (transclusionScope) {
-                    transclusionScope.$destroy();
-                }
-                angular.forEach(transcludedContent, function(el) {
-                    el.remove();
-                });
             }
         }
     }
