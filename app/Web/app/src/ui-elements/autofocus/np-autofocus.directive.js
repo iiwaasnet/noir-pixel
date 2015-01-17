4cb614f9c048b9633a78228e904a63072a4fc756
iiwaasnet <givasena@gmail.com>
2015-01-17 15:11:31 +0100

    autofocus directive

---
+++ app/Web/app/src/ui-elements/autofocus/np-autofocus.directive.js
@@ -0,0 +1,23 @@
+﻿(function(){
+    'use strict';
+
+    angular.module('np.ui-elements')
+        .directive('npAutofocus', autofocus);
+
+    autofocus.$inject = ['$timeout'];
+
+    function autofocus($timeout) {
+        var dir = {
+            restrict: 'A',
+            link: link
+        };
+
+        return dir;
+
+        function link(scope, element) {
+            $timeout(function() {
+                element[0].focus();
+            }, 100);
+        }
+    }
+})();
