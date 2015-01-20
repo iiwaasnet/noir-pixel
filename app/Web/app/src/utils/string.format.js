8e0d5568d1f41c5f211f1ff9bc965b59fae03041
iiwaasnet <givasena@gmail.com>
2015-01-20 20:43:20 +0100

    Changed from 'undefined' to undefined

--- app/Web/app/src/utils/string.format.js
+++ app/Web/app/src/utils/string.format.js
@@ -2,7 +2,7 @@
     String.prototype.format = function () {
         var args = arguments;
         return this.replace(/{(\d+)}/g, function (match, number) {
-            return typeof args[number] != 'undefined'
+            return typeof args[number] != undefined
               ? args[number]
               : match
             ;
@@ -16,7 +16,7 @@ if (!String.prototype.formatNamed) {
             var val = nameValueDict[key]
                 || nameValueDict[key.charAt(0).toUpperCase() + key.slice(1)]
                 || nameValueDict[key.charAt(0).toLowerCase() + key.slice(1)];
-            return val != 'undefined'
+            return val != undefined
               ? val
               : match
             ;
