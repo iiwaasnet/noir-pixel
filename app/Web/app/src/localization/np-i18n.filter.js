e75e305bf445c359e0d0947668cdf417a6ab0436
iiwaasnet <givasena@gmail.com>
2015-01-22 21:48:34 +0100

    Add trimming of the filter arguments

--- app/Web/app/src/localization/np-i18n.filter.js
+++ app/Web/app/src/localization/np-i18n.filter.js
@@ -13,12 +13,12 @@
             var values = item.split('|');
 
             if (values.length >= 1) {
-                var localizedTemplate = Strings.getLocalizedString(values[0]);
+                var localizedTemplate = Strings.getLocalizedString(values[0].trim());
                 if ((localizedTemplate !== null) && (localizedTemplate !== undefined) && (localizedTemplate !== '')) {
                     if (values.length > 1) {
                         for (var index = 1; index < values.length; index++) {
                             var target = '{' + (index - 1) + '}';
-                            localizedTemplate = localizedTemplate.replace(target, values[index]);
+                            localizedTemplate = localizedTemplate.replace(target, values[index].trim());
                         }
                     }
                     return localizedTemplate;
