c660218793ee244916af95511ab4b80b5266d18e
iiwaasnet <givasena@gmail.com>
2015-01-23 21:55:04 +0100

    Profile image upload

--- app/Web/app/src/app/np.module.js
+++ app/Web/app/src/app/np.module.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('np', ['ng', 'ngAnimate', 'ngSanitize', 'ui.router', 'ngMessages', 'ui.select',
-
+        'flow',
         'np.progress', 'np.logging', 'np.i18n', 'np.auth', 'np.home', 'np.layout', 'np.messages',
         'np.ui-elements', 'np.utils', 'np.validation', 'np.events', 'np.images',
         'np.user', 'np.roles', 'np.view-resolver', 'np.user-home', 'np.const', 'np.profile'
