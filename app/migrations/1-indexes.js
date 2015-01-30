89ca59b27d05506e811ea25194667f4a7ed7a1a3
iiwaasnet <givasena@gmail.com>
2015-01-30 19:32:24 +0100

    MongoDb migrations framework

---
+++ app/migrations/1-indexes.js
@@ -0,0 +1,11 @@
+module.exports.id = "indexes";
+
+module.exports.up = function(done) {
+    var profiles = this.db.collection('profiles');
+    profiles.ensureIndex({ 'UserId': 1 }, {unique: true, name: 'udx_user_id'}, done);
+};
+
+module.exports.down = function(done) {
+    var profiles = this.db.collection('profiles');
+    profiles.dropIndex('udx_user_id', done);
+};
