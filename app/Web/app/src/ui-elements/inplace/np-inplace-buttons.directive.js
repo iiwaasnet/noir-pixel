0bea4b6fabff86b07e415b15c2785a8c2ff42022
iiwaasnet <givasena@gmail.com>
2015-02-04 22:00:48 +0100

    Inplace buttons

---
+++ app/Web/app/src/ui-elements/inplace/np-inplace-buttons.directive.js
@@ -0,0 +1,31 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('np.ui-elements')
+        .directive('npInplaceButtons', inplaceButtons);
+
+    function inplaceButtons() {
+        var dir = {
+            restrict: 'AE',
+            templateUrl: '/app/src/ui-elements/inplace/inplace-buttons.html',
+            transclude: true,
+            link: link,
+            scope: {
+                inplaceButtonsYes: '@'
+            }
+        };
+
+        return dir;
+
+        function link(scope, element) {
+            var transcluded = angular.element(element[0].('ng-transclude')[0]);
+            var inlineButtons = angular.element(element[0].getElementsByClassName('inline-buttons'));
+            transcluded.on('click', onClick);
+
+            function onClick() {
+                transcluded.addClass('hidden');
+                inlineButtons.removeClass('hidden');
+            }
+        }
+    }
+})();
