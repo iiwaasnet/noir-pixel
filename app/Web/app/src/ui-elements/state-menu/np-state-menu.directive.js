a3f352e71063b4981ac5b7ae97046a5652b8c00f
iiwaasnet <givasena@gmail.com>
2015-02-11 22:40:05 +0100

    Darkroom

---
+++ app/Web/app/src/ui-elements/state-menu/np-state-menu.directive.js
@@ -0,0 +1,30 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('np.ui-elements')
+        .directive('npStateMenu', stateMenu);
+
+    function stateMenu() {
+        var dir = {
+            restrict: 'A',
+            controller: controller
+        };
+
+        return dir;
+
+        function controller() {
+            var ctrl = this,
+                items = [];
+            ctrl.resetAll = resetAll;
+            ctrl.addItem = addItem;
+
+            function addItem(item) {
+                items.push(item);
+            }
+
+            function resetAll() {
+                angular.forEach(items, function(item) { item.reset(); });
+            }
+        }
+    }
+})();
