104f38c369f19adb463117ca28c6efc2db3da4e1
iiwaasnet <givasena@gmail.com>
2015-02-12 20:49:13 +0100

    Darkroom

---
+++ app/Web/app/src/ui-elements/state-menu/np-state-menu-item.directive.js
@@ -0,0 +1,71 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('np.ui-elements')
+        .directive('npStateMenuItem', stateMenuItem);
+
+    function stateMenuItem() {
+        var dir = {
+            restrict: 'A',
+            require: '^npStateMenu',
+            link: link,
+            scope: {}
+        };
+
+        return dir;
+
+        function link(scope, element, attrs, stateMenuCtrl) {
+            var selectedClassName = 'selected';
+            var disabledClassName = 'disabled';
+            var subMenu = angular.element(element[0].querySelector('*[np-state-submenu]'));
+            scope.reset = reset;
+            scope.disable = disable;
+            scope.enable = enable;
+
+            stateMenuCtrl.addItem(scope);
+            enable();
+            element.on('$destroy', cleanup);
+
+            function click(e) {
+                e.stopPropagation();
+                var isSet = element.hasClass(selectedClassName);
+                stateMenuCtrl.resetAll();
+                if (!isSet) {
+                    set();
+                } else {
+                    stateMenuCtrl.enableAll();
+                }
+            }
+
+            function set() {
+                enable();
+                element.addClass(selectedClassName);
+                if (subMenu) {
+                    subMenu.addClass(selectedClassName);
+                }
+            }
+
+            function enable() {
+                element.removeClass(disabledClassName);
+                element.on('click', click);
+            }
+
+            function disable() {
+                element.addClass(disabledClassName);
+                element.off('click', click);
+            }
+
+            function reset() {
+                disable();
+                if (subMenu) {
+                    subMenu.removeClass(selectedClassName);
+                }
+                element.removeClass(selectedClassName);
+            }
+
+            function cleanup() {
+                element.off('click', click);
+            }
+        }
+    }
+})();
--- app/Web/app/src/ui-elements/state-menu/np-state-menu.directive.js
+++ app/Web/app/src/ui-elements/state-menu/np-state-menu.directive.js
@@ -16,6 +16,7 @@
             var ctrl = this,
                 items = [];
             ctrl.resetAll = resetAll;
+            ctrl.enableAll = enableAll;
             ctrl.addItem = addItem;
 
             function addItem(item) {
@@ -24,6 +25,10 @@
 
             function resetAll() {
                 angular.forEach(items, function(item) { item.reset(); });
+            }
+
+            function enableAll() {
+                angular.forEach(items, function (item) { item.enable(); });
             }
         }
     }
