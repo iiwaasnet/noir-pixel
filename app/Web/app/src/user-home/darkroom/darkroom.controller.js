ecc3148b0ae0d5ac6f9ee113eb346391d7d2b659
iiwaasnet <givasena@gmail.com>
2015-02-13 20:49:00 +0100

    Darkroom

--- app/Web/app/src/user-home/darkroom/darkroom.controller.js
+++ app/Web/app/src/user-home/darkroom/darkroom.controller.js
@@ -4,9 +4,23 @@
     angular.module('np.user-home')
         .controller('DarkroomController', darkroomController);
 
-    //darkroomController.$inject = [''];
+    darkroomController.$inject = ['$scope', 'Url', 'Config'];
 
-    function darkroomController() {
-        
+    function darkroomController($scope ,Url, Config) {
+        var ctrl = this;
+        ctrl.progress = progress;
+        ctrl.imageUpload = getImageUploadConfig();
+        ctrl.loadProgress = 0;
+
+        function progress(loaded) {
+            ctrl.loadProgress = loaded * 100;
+        }
+
+        function getImageUploadConfig() {
+            var config = {
+                endpoint: Url.build(Config.ApiUris.Base, Config.ApiUris.Profiles.UpdateProfileImage)
+            };
+            return config;
+        }
     }
 })();
