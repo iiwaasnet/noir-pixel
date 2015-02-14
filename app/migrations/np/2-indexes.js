92341efe5b887dcd11faacdcfd05d34dad2b753b
iiwaasnet <givasena@gmail.com>
2015-02-14 21:52:49 +0100

    Formatting

--- app/migrations/np/2-indexes.js
+++ app/migrations/np/2-indexes.js
@@ -1,7 +1,7 @@
 module.exports.id = "2-indexes";
 
 module.exports.up = function (done) {
- var profiles = this.db.collection('countries');
+	var profiles = this.db.collection('countries');
     profiles.ensureIndex({ 'Code': 1 }, {unique: true, name: 'udx_code'}, done);
 };
 
