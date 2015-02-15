b9367e5e9c64ad37690aaa90cb1f2c8ac7c96286
iiwaasnet <givasena@gmail.com>
2015-02-15 20:50:34 +0100

    Spinner directive

---
+++ app/Web/app/src/ui-elements/spinner/np-spinner.directive.js
@@ -0,0 +1,15 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('np.ui-elements')
+        .directive('npSpinner', spinner);
+
+    function spinner() {
+        var dir = {
+            restrict: 'AE',
+            templateUrl: '/app/src/ui-elements/spinner/spinner.html'
+        };
+
+        return dir;
+    }
+})();
