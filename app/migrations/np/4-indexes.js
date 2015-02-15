b0e09fa5b6d1888021f29ba2dca1462045873d3c
iiwaasnet <givasena@gmail.com>
2015-02-15 21:17:38 +0100

    Removed uniqueness for OwnerId-Status index on Photos collection

--- app/migrations/np/4-indexes.js
+++ app/migrations/np/4-indexes.js
@@ -2,10 +2,10 @@ module.exports.id = "4-indexes";
 
 module.exports.up = function (done) {
   var profiles = this.db.collection('photos');
-    profiles.ensureIndex({ 'OwnerId': 1, 'Status': 1 }, {unique: true, name: 'udx_ownerId_status'}, done);
+    profiles.ensureIndex({ 'OwnerId': 1, 'Status': 1 }, {name: 'idx_ownerId_status'}, done);
 };
 
 module.exports.down = function (done) {
   var profiles = this.db.collection('photos');
-    profiles.dropIndex('udx_ownerId_status', done);
+    profiles.dropIndex('idx_ownerId_status', done);
 };
