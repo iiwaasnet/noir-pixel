4838efe3ed6b36684b8450fc812c15130e5d883b
iiwaasnet <givasena@gmail.com>
2015-02-17 20:31:20 +0100

    arc-progress added

--- app/Web/app/src/app/np.module.js
+++ app/Web/app/src/app/np.module.js
@@ -1,11 +1,39 @@
 ﻿(function() {
     'use strict';
 
-    angular.module('np', ['ng', 'ngAnimate', 'ngSanitize', 'ui.router', 'ngMessages', 'ui.select',
+    var vendor = [
+        'ng',
+        'ngAnimate',
+        'ngSanitize',
+        'ui.router',
+        'ngMessages',
+        'ui.select',
         'flow',
-        'np.progress', 'np.logging', 'np.i18n', 'np.auth', 'np.home', 'np.layout', 'np.messages',
-        'np.ui-elements', 'np.utils', 'np.validation', 'np.events', 'np.images',
-        'np.user', 'np.roles', 'np.view-resolver', 'np.user-home', 'np.const', 'np.profile', 'np.geo',
+        'angular-progress-arc'
+    ];
+
+    var app = [
+        'np.progress',
+        'np.logging',
+        'np.i18n',
+        'np.auth',
+        'np.home',
+        'np.layout',
+        'np.messages',
+        'np.ui-elements',
+        'np.utils',
+        'np.validation',
+        'np.events',
+        'np.images',
+        'np.user',
+        'np.roles',
+        'np.view-resolver',
+        'np.user-home',
+        'np.const',
+        'np.profile',
+        'np.geo',
         'np.photos'
-        ]);   
+    ];
+
+    angular.module('np', vendor.concat(app));
 })();
