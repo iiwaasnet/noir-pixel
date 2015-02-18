7fa4495cbcddcb5c8e9f5ff620d16069e043e617
iiwaasnet <givasena@gmail.com>
2015-02-18 22:06:20 +0100

    Image crop directive

--- app/Web/app/src/ui-elements/image-crop/np-image-crop.directive.js
+++ app/Web/app/src/ui-elements/image-crop/np-image-crop.directive.js
@@ -17,22 +17,33 @@
             if (element[0].tagName !== 'IMG') {
                 return;
             }
-            var imageSrc = attrs.ngSrc || attrs.src;
-            var inMem = angular.element('<img/>');
-            inMem.on('load', imageLoaded);
-            inMem.attr('src', imageSrc);
 
-            function imageLoaded() {
+            var inMem = angular.element('<img/>');
+            var imageSrc = attrs.ngSrc || attrs.src;
+            if (!imageSrc) {
+                attrs.$observe('src', loadImage);
+            } else {
+                loadImage(imageSrc);
+            }
+
+            function loadImage(src) {
+                debugger;
+                inMem.on('load', cropImage);
+                inMem.attr('src', src);
+            }
+
+            function cropImage() {
+                debugger;
                 var cropMarginWidth = 5,
-                    canvas = angular.element('<canvas/>')
+                    canvas = angular
+                        .element('<canvas/>')
                         .attr({
                             width: inMem.width - 2 * cropMarginWidth,
                             height: inMem.height - 2 * cropMarginWidth
                         })
-                        .hide()
+                        .css('display', 'none')
                         .appendTo('body'),
                     ctx = canvas.get(0).getContext('2d'),
-                    a = $('<a download="cropped-image" title="click to download the image" />'),
                     cropCoords = {
                         topLeft: {
                             x: cropMarginWidth,
@@ -47,18 +58,6 @@
                 ctx.drawImage(inMem, cropCoords.topLeft.x, cropCoords.topLeft.y, cropCoords.bottomRight.x, cropCoords.bottomRight.y, 0, 0, img.width, img.height);
                 var base64ImageData = canvas.get(0).toDataURL();
                 element.attr('href', base64ImageData);
-
-                //a
-                //    .attr('href', base64ImageData)
-                //    .text('cropped image')
-                //    .appendTo('body');
-
-                //a
-                //    .clone()
-                //    .attr('href', img.src)
-                //    .text('original image')
-                //    .attr('download', 'original-image')
-                //    .appendTo('body');
 
                 canvas.remove();
             }
