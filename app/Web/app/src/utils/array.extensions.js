4a2de46e80fa779c488f12448a5fc5106b17bfa9
iiwaasnet <givasena@gmail.com>
2015-02-23 21:00:26 +0100

    Array extension methods

---
+++ app/Web/app/src/utils/array.extensions.js
@@ -0,0 +1,22 @@
+﻿if (!Array.prototype.first) {
+    Array.prototype.first = function(predicate) {
+        'use strict';
+
+        for (var i = 0; i < this.length; i++) {
+            var current = this[i];
+            if (predicate(current)) {
+                return current;
+            }
+        }
+
+        return undefined;
+    };
+}
+
+if (!Array.prototype.any) {
+    Array.prototype.any = function (predicate) {
+        'use strict';
+
+        return this.first(predicate) != undefined;
+    };
+}
