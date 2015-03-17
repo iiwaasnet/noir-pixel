c6693264fef9c3910f8db26963ce10898963029c
sivasenko <sergey.ivasenko@bwinparty.com>
2015-03-17 09:18:53 +0100

    Rename Mongo indexes !Drop collections and migration data

--- app/migrations/np-identity/1-indexes.js
+++ app/migrations/np-identity/1-indexes.js
@@ -2,10 +2,10 @@ module.exports.id = "1-indexes";
 
 module.exports.up = function (done) {
     var profiles = this.db.collection('users');
-    profiles.ensureIndex({ 'UserName': 1 }, { unique: true, name: 'udx_user_name' }, done);
+    profiles.ensureIndex({ 'UserName': 1 }, { unique: true, name: 'udx_userName' }, done);
 };
 
 module.exports.down = function (done) {
     var profiles = this.db.collection('users');
-    profiles.dropIndex('udx_user_name', done);
+    profiles.dropIndex('udx_userName', done);
 };
--- app/migrations/np/1-indexes.js
+++ app/migrations/np/1-indexes.js
@@ -2,10 +2,10 @@ module.exports.id = "1-indexes";
 
 module.exports.up = function(done) {
     var profiles = this.db.collection('profiles');
-    profiles.ensureIndex({ 'UserId': 1 }, {unique: true, name: 'udx_user_id'}, done);
+    profiles.ensureIndex({ 'UserId': 1 }, {unique: true, name: 'udx_userId'}, done);
 };
 
 module.exports.down = function(done) {
     var profiles = this.db.collection('profiles');
-    profiles.dropIndex('udx_user_id', done);
+    profiles.dropIndex('udx_userId', done);
 };
